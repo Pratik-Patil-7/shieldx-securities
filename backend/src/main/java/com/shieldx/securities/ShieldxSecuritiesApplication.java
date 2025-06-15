f35cf5ad0da05c063f80a8208cabe8c9fc3435a5
Pratik-Patil-7 <108pratikmpatil@gmail.com>
2025-06-15 17:19:57 +0530

    initial commit

---
+++ backend/src/main/java/com/shieldx/securities/ShieldxSecuritiesApplication.java
@@ -0,0 +1,13 @@
+package com.shieldx.securities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldxSecuritiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldxSecuritiesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shieldx/securities/ShieldxSecuritiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shieldx.securities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldxSecuritiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
