169d6e5ba2051a064ab92b533b8579f49ebacaeb
Pratik-Patil-7 <108pratikmpatil@gmail.com>
2025-06-17 22:18:10 +0530

    no changes

--- backend/src/main/java/com/shieldx/securities/controller/UserController.java
+++ backend/src/main/java/com/shieldx/securities/controller/UserController.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shieldx.securities.dto.UserDTO;
+import com.shieldx.securities.service.EmailService;
 import com.shieldx.securities.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
-	
+
 	@PostMapping("/register")
 	public String register(@RequestBody UserDTO userDTO) {
 		return userService.registerUser(userDTO);
